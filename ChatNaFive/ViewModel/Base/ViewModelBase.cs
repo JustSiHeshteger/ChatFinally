c0fd10c4ca76cabd959076be5f42e8e675fcbf85
Андрей Стригалев <siheshteger@gmail.com>
2022-03-15 14:22:34 +0300

    Add project files.

---
+++ ChatNaFive/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatNaFive.ViewModel.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null) //Посмотри в классе MainWindowViewModel
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            else
+            {
+                field = value;
+                OnPropertyChanged(PropertyName);
+                return true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _isDisposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _isDisposed)
+            {
+                return;
+            }
+            else
+            {
+                _isDisposed = true;
+            }
+        }
+    }
+}
---
+++ ChatNaFive/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ChatNaFive.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatNaFive.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string _title;
+
+        public string Title
+        {
+            get => _title;
+
+            set => Set(ref _title, value); //Это то, что написали
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+
+            set => Set(ref _title, value); //Это то, что написали
+        }
+
+        private IEnumerable<string> _messages;
+
+        public IEnumerable<string> Messages
+        {
+            get => _messages;
+            set
+            {
+            }
+        }
+    }
+}

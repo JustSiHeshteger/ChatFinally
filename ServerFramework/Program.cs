3dc8c7ca4cda6337d883cf6f21d1ed6b46a5ec50
Андрей Стригалев <siheshteger@gmail.com>
2022-05-13 11:48:17 +0300

    сервер теперь поддерживает русский язык

--- ServerFramework/Program.cs
+++ ServerFramework/Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
+using System.Text;
 
 namespace ServerFramework
 {
     class Program
     {
-        static ServerObject server; // сервер
-        static Thread listenThread; // потока для прослушивания
+        static ServerObject server; 
+        static Thread listenThread; 
         static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8;
 
             try
             {
                 server = new ServerObject();
                 listenThread = new Thread(new ThreadStart(server.Listen));
-                listenThread.Start(); //старт потока
+                listenThread.Start(); 
             }
             catch (Exception ex)
             {
